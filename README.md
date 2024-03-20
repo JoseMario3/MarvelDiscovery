aee1adf20f9baaf55d6b8c07b77ee4fd2cb6de82
Jose Folgar <f.jose@wustl.edu>
2024-03-20 01:30:53 -0500

    Project Complete
    A little ugly but it'll suffice

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MarvelDiscovery
  AND 101 Project 5: Find new Marvel characters using Marvel's API
+ dd3e7d9e4be1dbd16195aaad7d9f0b41
--- app/src/main/java/com/example/marveldiscovery/MainActivity.kt
+++ app/src/main/java/com/example/marveldiscovery/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.marveldiscovery
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONArray
+import org.w3c.dom.Text
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var resultsArray: JSONArray? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,66 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val name = findViewById<TextView>(R.id.name)
+        val pic = findViewById<ImageView>(R.id.image)
+        val desc = findViewById<TextView>(R.id.description)
+        val button = findViewById<Button>(R.id.button)
+        getCharacters()
+        getNextCharacter(button, pic, name, desc)
+    }
+
+    private fun getCharacters() {
+        val client = AsyncHttpClient()
+        val timestamp = System.currentTimeMillis()
+        val publicKey = "dd3e7d9e4be1dbd16195aaad7d9f0b41"
+        val privateKey = "f240c2909f1a1a2e8f0141ec2f3b09206a758048"
+        val hash = stringToMd5("$timestamp$privateKey$publicKey")
+
+        val url =
+            "https://gateway.marvel.com/v1/public/characters?ts=$timestamp&apikey=$publicKey&hash=$hash"
+        client[url, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Marvel", "response successful $json")
+                resultsArray = json.jsonObject.optJSONObject("data")?.getJSONArray("results")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Marvelous Error", errorResponse)
+            }
+        }]
+    }
+
+    private fun getNextCharacter(button: Button, image: ImageView, name: TextView, desc: TextView) {
+        button.setOnClickListener {
+            val choice = Random.nextInt(19)
+            val character = resultsArray?.getJSONObject(choice)
+            val thumbnail = character?.getJSONObject("thumbnail")
+            val path = thumbnail?.getString("path")?.substring(7)
+            val extension = thumbnail?.getString("extension")
+            val imageUrl = "https://$path.$extension"
+            val charName = character?.getString("name")
+            val charDesc = character?.getString("description")
+            Log.d("Hello", imageUrl)
+            Glide.with(this)
+                .load(imageUrl)
+                .into(image)
+            name.text = charName
+            if (charDesc != "") {
+                desc.text = charDesc
+            } else {
+                desc.text = "Uh Oh! No description for this character :("
+            }
+        }
+    }
+
+    private fun stringToMd5(s: String): Any {
+        val md = MessageDigest.getInstance("MD5")
+        val bigInt = BigInteger(1, md.digest(s.toByteArray(Charsets.UTF_8)))
+        return String.format("%032x", bigInt)
     }
 }
